732fbcd1ed8d3cfad89dd41f3aabd50e1693d1f0
wenjie1 <330929643@qq.com>
2019-05-07 17:30:25 +0800

    地图可以显示了

--- app/src/main/java/com/android/mygaode/MainActivity.java
+++ app/src/main/java/com/android/mygaode/MainActivity.java
@@ -3,21 +3,60 @@ package com.android.mygaode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    MapView mapView;
+    AMap aMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//第一次创建项目
-        //开始
+        //初始化
         initView();
+        mapView.onCreate(savedInstanceState);
 
     }
 
     private void initView() {
+        mapView=findViewById(R.id.mapview);
 
-        //初始化
+
+        if (aMap == null) {
+            aMap = mapView.getMap();
+        }
 
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mapView.onSaveInstanceState(outState);
+    }
+
 
 }
