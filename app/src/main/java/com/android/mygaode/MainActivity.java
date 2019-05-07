07f228db6a694e3589e2f71d6a2f6ad4eb6f27e2
wenjie1 <330929643@qq.com>
2019-05-07 13:55:29 +0800

    第一次提交，初始化

--- app/src/main/java/com/android/mygaode/MainActivity.java
+++ app/src/main/java/com/android/mygaode/MainActivity.java
@@ -8,6 +8,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//第一次创建项目
+        //开始
+        initView();
+
     }
+
+    private void initView() {
+
+        //初始化
+
+    }
+
 }
