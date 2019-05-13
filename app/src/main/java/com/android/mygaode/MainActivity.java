44f70cf1c60930d7339860f61ccb72664a5cfc26
wenjie1 <330929643@qq.com>
2019-05-13 17:55:04 +0800

    左边新增滑动菜单，地图定位

--- app/src/main/java/com/android/mygaode/MainActivity.java
+++ app/src/main/java/com/android/mygaode/MainActivity.java
@@ -1,37 +1,124 @@
 package com.android.mygaode;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.route.RouteSearch;
+import com.android.mygaode.weight.StatusBarUtils;
+import com.android.mygaode.weight.StatusbarColorUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  LocationSource, AMapLocationListener,View.OnClickListener {
+
+    // 抽屉菜单对象
+    private ActionBarDrawerToggle drawerbar;
+    LinearLayout main_left_drawer_layout;
+    DrawerLayout drawerLayout;
+
+    ImageView img_menu;
 
     MapView mapView;
     AMap aMap;
+
+    //北京市的经纬度   默认进来是北京，后面定位成功以后，中心点始终是自己
+    private LatLng centerWHpoint = new LatLng(39.910066, 116.386695);
+
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption = null;
+    private LocationSource.OnLocationChangedListener mListener;
+
+    //定位蓝点
+    MyLocationStyle myLocationStyle;
+    private UiSettings mUiSettings;//定义一个UiSettings对象
+
+    //自己的纬度
+    double mylat;
+    //自己的经度
+    double mylng;
+
+    LatLng mylatlng;//自己的经纬度
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//第一次创建项目
+
+
+        requestPermissions();//动态申请权限
+
         //初始化
         initView();
         mapView.onCreate(savedInstanceState);
+
+//        StatusBarUtils.with(this)
+//                .init();
+        StatusBarUtils.setStatusTransparent(this);
 
     }
 
     private void initView() {
         mapView=findViewById(R.id.mapview);
+        //上面的菜单按钮
+        img_menu=findViewById(R.id.img_menu);
+        img_menu.setOnClickListener(this);
 
+        drawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
+        //设置菜单内容之外其他区域的背景色
+        drawerLayout.setScrimColor(Color.TRANSPARENT);
 
-        if (aMap == null) {
-            aMap = mapView.getMap();
-        }
+        //左边菜单
+        main_left_drawer_layout = findViewById(R.id.main_left_drawer_layout);
+        //左边菜单
+        initEvent();
+
+       mapInit();
 
     }
+
+
+
+
+
+
+
+
+
 
     @Override
     protected void onDestroy() {
@@ -59,4 +146,287 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        mListener = onLocationChangedListener;
+        //初始化定位
+        init();
+    }
+
+    @Override
+    public void deactivate() {
+        mListener = null;
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation amapLocation) {
+        //解析AMapLocation对象
+        //首先，可以判断AMapLocation对象不为空，当定位错误码类型为0时定位成功。
+        if (amapLocation != null) {
+            if (amapLocation.getErrorCode() == 0) {
+                mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+
+                //可在其中解析amapLocation获取相应内容。
+                amapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                amapLocation.getLatitude();//获取纬度
+                amapLocation.getLongitude();//获取经度
+                amapLocation.getAccuracy();//获取精度信息
+                String address = amapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
+                String country = amapLocation.getCountry();//国家信息
+                amapLocation.getProvince();//省信息
+                amapLocation.getCity();//城市信息
+                amapLocation.getDistrict();//城区信息
+                amapLocation.getStreet();//街道信息
+                amapLocation.getStreetNum();//街道门牌号信息
+                amapLocation.getCityCode();//城市编码
+                amapLocation.getAdCode();//地区编码
+                amapLocation.getAoiName();//获取当前定位点的AOI信息
+                amapLocation.getBuildingId();//获取当前室内定位的建筑物Id
+                amapLocation.getFloor();//获取当前室内定位的楼层
+                //amapLocation.getGpsStatus();//获取GPS的当前状态
+                amapLocation.getGpsAccuracyStatus();
+                //获取定位时间
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date date = new Date(amapLocation.getTime());
+                String format = df.format(date);
+
+                mylat = amapLocation.getLatitude();//自己的纬度
+                mylng = amapLocation.getLongitude();//自己的经度
+
+                 mylatlng=new LatLng(mylat,mylng);
+
+                 centerWHpoint=mylatlng;
+
+                aMap.animateCamera(CameraUpdateFactory.changeLatLng(mylatlng));
+
+
+                Log.e("qqqq", "地址:" + address + "----国家信息:" + country + "----time--> " + format);
+
+            } else {
+                //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                Log.e("qqqq", "location Error, ErrCode:"
+                        + amapLocation.getErrorCode() + ", errInfo:"
+                        + amapLocation.getErrorInfo());
+            }
+        }
+    }
+
+    private void init() {
+        //初始化定位
+        mLocationClient = new AMapLocationClient(this.getApplicationContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(this);
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+        //--------------------------------------------------
+        //选择定位模式
+        //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置单次定位
+        //获取一次定位结果：
+        //该方法默认为false。
+        mLocationOption.setOnceLocation(true);
+        //获取最近3s内精度最高的一次定位结果：
+        //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+        mLocationOption.setOnceLocationLatest(true);
+        //--------------------------------------------------
+        //自定义连续定位
+        //SDK默认采用连续定位模式，时间间隔2000ms。如果您需要自定义调用间隔：
+        //设置定位间隔,单位毫秒,默认为2000ms，最低1000ms。
+        mLocationOption.setInterval(1000);
+        //--------------------------------------------------
+        //其他参数
+        //设置是否返回地址信息（默认返回地址信息）
+        mLocationOption.setNeedAddress(true);
+        //设置是否允许模拟软件Mock位置结果，多为模拟GPS定位结果，默认为true，允许模拟位置。
+        //设置是否允许模拟位置,默认为true，允许模拟位置
+        mLocationOption.setMockEnable(true);
+        //设置定位请求超时时间，默认为30秒。
+        //单位是毫秒，默认30000毫秒，建议超时时间不要低于8000毫秒。
+        mLocationOption.setHttpTimeOut(8000);
+        //设置是否开启定位缓存机制
+        //缓存机制默认开启，可以通过以下接口进行关闭。
+        //当开启定位缓存功能，在高精度模式和低功耗模式下进行的网络定位结果均会生成本地缓存，不区分单次定位还是连续定位。GPS定位结果不会被缓存。
+        //关闭缓存机制
+        mLocationOption.setLocationCacheEnable(true);
+        //--------------------------------------------------
+        //启动定位
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        mLocationClient.startLocation();
+    }
+    //动态申请权限
+    private void requestPermissions() {
+
+        List<String> permissionList = new ArrayList<>();
+//        允许程序打开网络套接字
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.INTERNET);
+        }
+//        允许程序设置内置sd卡的写权限
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+//        允许程序获取网络状态
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_NETWORK_STATE);
+        }
+//                允许程序访问WiFi网络信息
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_WIFI_STATE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_WIFI_STATE);
+        }
+//        允许程序读写手机状态和身份
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+//            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+//        }
+//                允许程序访问CellID或WiFi热点来获取粗略的位置
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+        //申请打电话权限
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+//            permissionList.add(Manifest.permission.CALL_PHONE);
+//        }
+
+        if (!permissionList.isEmpty()) {  //申请的集合不为空时，表示有需要申请的权限
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(permissionList.toArray(new String[permissionList.size()]), 1);
+            }
+        } else { //所有的权限都已经授权过了
+
+        }
+    }
+
+    //动态申请权限部分
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0) { //安全写法，如果小于0，肯定会出错了
+                    for (int i = 0; i < grantResults.length; i++) {
+
+                        int grantResult = grantResults[i];
+                        if (grantResult == PackageManager.PERMISSION_DENIED) { //这个是权限拒绝
+                            String s = permissions[i];
+//                            Toast.makeText(this, s + "权限被拒绝了", Toast.LENGTH_SHORT).show();
+//                            T.showToastSafeError("暂未获取到定位信息");
+
+
+                            //判断用户是否拒绝了定位权限
+                            PackageManager pm = getPackageManager();
+                            boolean flag = (PackageManager.PERMISSION_GRANTED ==
+                                    pm.checkPermission("android.permission.ACCESS_COARSE_LOCATION", "packageName"));
+
+                            if (flag) {
+                                //有这个权限，做相应处理
+                            }else { //没有权限
+
+
+
+                            }
+
+
+                        } else { //授权成功了
+                            //do Something
+                            mapInit();
+
+                        }
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    //地图初始化
+    private void mapInit() {
+        aMap = mapView.getMap();
+
+//        aMap.moveCamera(CameraUpdateFactory.changeLatLng(centerWHpoint));//默认先定位武汉市经纬度
+        aMap.animateCamera(CameraUpdateFactory.newLatLngZoom(centerWHpoint, 14));//默认先定位武汉市经纬度
+        if (aMap != null) {
+            //设置显示定位按钮 并且可以点击
+            UiSettings settings = aMap.getUiSettings();
+            aMap.setLocationSource(this);//设置了定位的监听,这里要实现LocationSource接口
+            // 是否显示定位按钮
+            settings.setMyLocationButtonEnabled(false);
+
+            //设置地图的放缩级别
+
+
+
+            // 设置定位监听
+            aMap.setLocationSource(this);
+            // 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+            aMap.setMyLocationEnabled(true);//显示定位层并且可以触发定位,默认是flase
+        }
+
+
+        //蓝点初始化
+        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+
+        myLocationStyle.myLocationIcon(BitmapDescriptorFactory
+                .fromResource(R.mipmap.mylocation));// 设置小蓝点的图标
+
+        //aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
+        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        //myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）默认执行此种模式。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_SHOW);//只定位一次
+//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE) ;//定位一次，且将视角移动到地图中心点。
+        myLocationStyle.showMyLocation(true);
+
+
+        //隐藏定位范围的圈圈
+        myLocationStyle.strokeColor(Color.argb(0, 0, 0, 0));// 设置圆形的边框颜色
+        myLocationStyle.radiusFillColor(Color.argb(0, 0, 0, 0));// 设置圆形的填充颜色
+
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+
+
+        mUiSettings = aMap.getUiSettings();//实例化UiSettings类对象
+        mUiSettings.setZoomControlsEnabled(false);//隐藏放大缩小按钮
+
+
+    }
+
+    //设置开关监听
+    private void initEvent() {
+        drawerbar = new ActionBarDrawerToggle(this, drawerLayout, R.mipmap.ic_launcher, R.string.open, R.string.close) {
+            //菜单打开
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            // 菜单关闭
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+
+        drawerLayout.setDrawerListener(drawerbar);
+    }
+
+    //左边菜单开关事件
+    public void openLeftLayout(View view) {
+        if (drawerLayout.isDrawerOpen(main_left_drawer_layout)) {
+            drawerLayout.closeDrawer(main_left_drawer_layout);
+        } else {
+            drawerLayout.openDrawer(main_left_drawer_layout);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.img_menu:
+                openLeftLayout(img_menu);
+                break;
+        }
+    }
 }
